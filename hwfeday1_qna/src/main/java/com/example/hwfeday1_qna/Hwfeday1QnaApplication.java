4beb9d484ad01a55df87eee5bf24516d33c2c58a
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-08-06 16:30:25 +0900

    day1hw1

---
+++ hwfeday1_qna/src/main/java/com/example/hwfeday1_qna/Hwfeday1QnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwfeday1_qna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hwfeday1QnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hwfeday1QnaApplication.class, args);
+	}
+
+}
