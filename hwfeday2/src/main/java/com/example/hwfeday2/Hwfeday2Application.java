d9ea4034467d8bfbb0bb57baeb2654682cf652bb
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-08-07 16:40:10 +0900

    day2hw3
    translate

---
+++ hwfeday2/src/main/java/com/example/hwfeday2/Hwfeday2Application.java
@@ -0,0 +1,13 @@
+package com.example.hwfeday2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hwfeday2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hwfeday2Application.class, args);
+	}
+
+}
---
+++ hwfeday2/src/test/java/com/example/hwfeday2/Hwfeday2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwfeday2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hwfeday2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
