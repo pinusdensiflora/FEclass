5e8014fa8b7eb659b19ade6aca06faf7601c8864
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-08-07 10:14:41 +0900

    day2
    modal

---
+++ boardexam/src/main/java/com/example/boardexam/BoardexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardexamApplication.class, args);
+	}
+
+}
---
+++ boardexam/src/test/java/com/example/boardexam/BoardexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
