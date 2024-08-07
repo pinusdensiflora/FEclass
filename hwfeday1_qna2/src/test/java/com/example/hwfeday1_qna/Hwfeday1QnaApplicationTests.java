f03ee667adaccb179313498ecce54cf33c840e6c
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-08-07 15:48:06 +0900

    day2

---
+++ hwfeday1_qna2/src/test/java/com/example/hwfeday1_qna/Hwfeday1QnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwfeday1_qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hwfeday1QnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
