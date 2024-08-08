79cf6c4d58599302f08aeb5464ae62e830b35d4b
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-08-08 18:04:01 +0900

    day3hw

---
+++ hwfeday3/src/main/java/com/examplehwfeday3/Hwfeday3Application.java
@@ -0,0 +1,13 @@
+package com.examplehwfeday3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hwfeday3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hwfeday3Application.class, args);
+	}
+
+}
