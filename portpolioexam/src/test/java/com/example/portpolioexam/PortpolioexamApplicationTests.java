7f861a27a7cd380a7032a9947c202b1523d56da8
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-08-09 14:25:47 +0900

    day4
    portfolio

---
+++ portpolioexam/src/test/java/com/example/portpolioexam/PortpolioexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portpolioexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortpolioexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
