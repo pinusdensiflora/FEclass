af77b00fc9d7357b8021655e414f16be1aba0824
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-08-09 16:54:31 +0900

    hwday4

---
+++ hwfeday4/src/test/java/com/example/hwfeday4/Hwfeday4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwfeday4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hwfeday4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
